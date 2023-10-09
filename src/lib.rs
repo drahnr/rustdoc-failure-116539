b1750d0dc5c767dd3a948102dd900ce3b79e9dde
Bernhard Schuster <bernhard@ahoi.io>
2023-10-09 14:45:49 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -16,24 +16,22 @@ pub type RealAccountId = <<RealSignature as Verify>::Signer as IdentifyAccount>:
 
 pub struct Pallet<T>(std::marker::PhantomData<T>);
 
-
 pub struct BaseEvent<T: ?Sized> {
-	_x: std::marker::PhantomData<T>,
+    _x: std::marker::PhantomData<T>,
 }
 
 pub mod inner {
-	pub trait Config {
-		type RuntimeEvent;
-		type AccountId;
-	}
+    pub trait Config {
+        type RuntimeEvent;
+        type AccountId;
+    }
 }
 pub trait IsType<T>: From<T> + Into<T> {}
 
 pub trait Config: inner::Config<AccountId = RealAccountId> {
-	type RuntimeEvent: From<BaseEvent<Self>>
-		+ IsType<<Self as inner::Config>::RuntimeEvent>;
+    type RuntimeEvent: From<BaseEvent<Self>> + IsType<<Self as inner::Config>::RuntimeEvent>;
 }
 
 pub struct GenesisConfig<T: Config> {
-	pub shelves: Vec<<T as inner::Config>::AccountId>,
+    pub shelves: Vec<<T as inner::Config>::AccountId>,
 }
