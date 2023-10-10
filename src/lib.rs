1fec3167866271f668ecf135373e17c193219a5c
Bernhard Schuster <bernhard@ahoi.io>
2023-10-10 23:38:28 +0200

    trim more

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
 pub trait IdentifyAccount {
     type AccountId;
 }
-pub trait Verify {
-    type Signer;
-}
-
-
-
 
 pub struct RealSigner {}
 
@@ -14,13 +8,7 @@ impl IdentifyAccount for RealSigner {
     type AccountId = u32;
 }
 
-pub struct RealSignature {}
-
-impl Verify for RealSignature {
-    type Signer = RealSigner;
-}
-
-pub type RealAccountId = <<RealSignature as Verify>::Signer as IdentifyAccount>::AccountId;
+pub type RealAccountId = <RealSigner as IdentifyAccount>::AccountId;
 
 pub mod inner {
     pub trait Config {
@@ -28,8 +16,7 @@ pub mod inner {
     }
 }
 
-pub trait Config: inner::Config<AccountId = RealAccountId> {
-}
+pub trait Config: inner::Config<AccountId = RealAccountId> {}
 
 pub struct GenesisConfig<T: Config> {
     pub shelves: Vec<<T as inner::Config>::AccountId>,
